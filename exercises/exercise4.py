28a6052f6020e549e381fd561f11fe639062b337
Emma White <ewhite79@hotmail.com>
2015-02-10 16:06:36 -0500

    Clean up Exercise 4
    deleted white space, added line spacing, changed dictionary name, used
    word list instead of counting to get number of words

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -1,53 +1,77 @@
-#create empty list
-line_list = []
 #store file name
 filename = "RomeoJuliet.txt"
+
 #Open file
-Romeo = open("RomeoJuliet.txt", "r")
+Romeo = open("RomeoJuliet.txt","r")
+
+#LINE COUNT
+#create empty list
+line_list = []
+
 #loop over Romeo
 for current_line in Romeo:
+
 	#put each line into list "line_list"
-    line_list.append( current_line )
-	#end loop
+    line_list.append(current_line)
+
+#end loop
+
 #count lines and print
 print "Number of lines:", len(line_list)
-#create variable for word count
-word_count=0
+
+#WORD COUNT  
+#create empty list for words
+word_list = []
+
 #loop over lines
 for line in line_list:
+
     #create temp variable to hold list of words
     words = line.split()
-    #add number of words to word count
-    word_count += len(words)
-	#end loop
+
+    #add individual words into word list
+    word_list.extend (words) 
+
+#end loop
+
 #print out word count
-print "Number of words:", word_count 
+print "Number of words:", len(word_list) 
+
+#CHARACTER COUNT
 #create variable for character count
 char_count=0	
-#loop over lines nad count characters
+
+#loop over lines 
 for line in line_list:
+
+	#count characters for each line
     char_count += len(line)
-	#end loop
+
+#end loop
+
 #print character count
 print "Number of characters:", char_count
-#make one list of all words. create empty list
-word_list = []
-#loop though by line, split, and put words into one list
-for line in line_list:
-    wordsagain = line.split()
-    word_list.extend (wordsagain) 
-    #end loop
-#test if I got it right
-print "Number of words take 2:", len(word_list)
-#make dictionary. create empty dictionary. 
-dictionary = {}
+
+#CREATE DICTIONARY
+#create empty dictionary. 
+shakes_dictionary = {}
+
 #loop through word by word
 for word in word_list:
+
 	#if word already there, add one to value
-    if word in dictionary:
-        dictionary[word]+=1
+    if word in shakes_dictionary:
+
+        shakes_dictionary[word]+=1
+
     #if word is new, set value to one
     else:
-        dictionary[word]=1
-    #end loop
-print(dictionary)
+
+        shakes_dictionary[word]=1
+
+    #end check to see if name is in dictionary
+
+#end dictionary-creation loop
+
+# print the dictionary
+print "Shakespeare dictionary:", shakes_dictionary
