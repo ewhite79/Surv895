72b61e3e15a7557d6b4d6b61b2c6f2bc04286ce3
Emma White <ewhite79@hotmail.com>
2015-03-09 15:18:03 -0400

    final version of exercise6 code

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -59,11 +59,11 @@ try:
 ##############################READ IN BIG FILE############################################
 
     # open the data file for reading.
-    with open( 'twitter_sample.txt', 'rb' ) as tab_delimited_file:
+    with open( 'tweet_sample.txt', 'rb' ) as tab_delimited_file:
 
         # feed the file to csv.reader to parse.
         tweet_reader = csv.reader( tab_delimited_file, dialect = "excel-tab" )
-            # delimiter = '\t', quotechar = '\"', strict = True, lineterminator = "\n" )
+            delimiter = '\t', quotechar = '\"', strict = True, lineterminator = "\n" )
 
         # loop over logical rows in the file.
         tweet_counter = 0
@@ -73,7 +73,7 @@ try:
             field_count = len( current_tweet_row )
 
             # print some info
-            # print( "====> line " + str( tweet_counter ) + " - " + str( field_count ) + " fields - text: " + '|||'.join( current_tweet_row ) )
+            print( "====> line " + str( tweet_counter ) + " - " + str( field_count ) + " fields - text: " + '|||'.join( current_tweet_row ) )
 
             # only do stuff after first row
             if ( tweet_counter > 1 ):
@@ -105,7 +105,7 @@ try:
                 tweet_display_urls_mentioned = unicode( current_tweet_row[ 23 ], "UTF-8" )
 
                 # print tweet ID
-                print ( "====> line " + str( tweet_counter ) + " - " + str( field_count ) + " Twitter Tweet ID = " + twitter_tweet_id )
+                print ( "====> line " + str( tweet_counter ) + " - " + str( field_count ) +# " Twitter Tweet ID = " + twitter_tweet_id )
 
                 # build INSERT statement
                 sql_insert_string = '''
@@ -168,7 +168,7 @@ try:
                 # execute the command.
                 cursor.execute( sql_insert_string, ( tweet_timestamp, twitter_tweet_id, tweet_text, tweet_language, twitter_user_twitter_id, twitter_user_screenname, user_followers_count, user_favorites_count, user_created, user_location, tweet_retweet_count, tweet_place, tweet_user_mention_count, tweet_users_mentioned_screennames, tweet_users_mentioned_ids, tweet_hashtag_mention_count, tweet_hashtags_mentioned, tweet_url_count, tweet_shortened_urls_mentioned, tweet_full_urls_mentioned, user_description, user_friends_count, user_statuses_count, tweet_display_urls_mentioned ) )
 
-                # commit.
+               # commit.
                 connection.commit()
 
             #-- END check to make sure we aren't the first row. --#
@@ -180,66 +180,65 @@ try:
 #############################FILL USER TABLE############################
 
     #declare variables
-#    user_set = None
-#    current_row = None
-#    sql_insert_user = ""
-
-#    user_set = cursor.execute(
-#    	'''
-#    	SELECT 
-#            twitter_user_twitter_id,
-#            twitter_user_screenname,
-#            user_followers_count,
-#            user_favorites_count,
-#            user_created,
-#            user_location,
-#            user_description,
-#            user_friends_count,
-#            user_statuses_count
-#        FROM tweet_sample_raw
-#        ''')
+    user_set = None
+    current_row = None
+    sql_insert_user = ""
+    user_set = cursor.execute(
+    	'''
+    	SELECT 
+            twitter_user_twitter_id,
+            twitter_user_screenname,
+            user_followers_count,
+            user_favorites_count,
+            user_created,
+            user_location,
+            user_description,
+            user_friends_count,
+            user_statuses_count
+        FROM tweet_sample_raw
+        ''')
     
-#    unique_users = []
+    unique_users = []
 
-   #loop through rows
-#    for current_row in user_set:
+    #loop through rows
+    for current_row in user_set:
 
-#        if current_row[0] not in unique_users:
+        if current_row[0] not in unique_users:
 
-#            unique_users.append(current_row[0])
+            unique_users.append(current_row[0])
 
-#            sql_insert_user = '''
-#                INSERT INTO User (
-#                    twitter_user_twitter_id,
-#                    twitter_user_screenname,
-#                    user_followers_count,
-#                    user_favorites_count,
-#                    user_created,
-#                    user_location,
-#                    user_description,
-#                    user_friends_count,
-#                    user_statuses_count
-#             	    )
-#                VALUES (?,?,?,?,?,?,?,?,?);
-#                '''
-        	#execute insert function
+            sql_insert_user = '''
+                INSERT INTO User (
+                    twitter_user_twitter_id,
+                    twitter_user_screenname,
+                    user_followers_count,
+                    user_favorites_count,
+                    user_created,
+                    user_location,
+                    user_description,
+                    user_friends_count,
+                    user_statuses_count
+             	    )
+                VALUES (?,?,?,?,?,?,?,?,?);
+                '''
+       		#execute insert function
 
-#            cursor2.execute(sql_insert_user,(current_row[0], current_row[1], current_row[2], 
-#        	    current_row[3], current_row[4],current_row[5], current_row[6], current_row[7],
-#        	    current_row[8]))
+            cursor2.execute(sql_insert_user,(current_row[0], current_row[1], current_row[2], 
+        	    current_row[3], current_row[4],current_row[5], current_row[6], current_row[7],
+        	    current_row[8]))
     	
             # get ID of each record.
-#            new_user_id = cursor2.lastrowid
+            new_user_id = cursor2.lastrowid
     
             # output result
-#            print("New record inserted with ID = " + str(new_user_id))
+            print("New record inserted with ID = " + str(new_user_id))
 
             #commit
-#            connection.commit()
+            connection.commit()
 
-#        else:
+        else:
 
-#            pass 
+            pass 
         
     #end loop
 
@@ -247,118 +246,119 @@ try:
     ############################FILL TWEET TABLE##########################
 
     #declare variables
-#    tweet_set = None
-#    current_row = None
-#    sql_insert_tweet = ""
+    tweet_set = None
+    current_row = None
+    sql_insert_tweet = ""
 
     #SELECT statement
-#    tweet_set = cursor.execute(
-#    	'''
-#        SELECT 
-#    	    twitter_user_twitter_id,
-#    	    tweet_timestamp,
-#            twitter_tweet_id,
-#            tweet_text,
-#            tweet_language,
-#            tweet_retweet_count,
-#            tweet_place,
-#            tweet_user_mention_count,
-#            tweet_hashtag_mention_count,
-#            tweet_url_count
-#        FROM tweet_sample_raw
-#        ''') 
+    tweet_set = cursor.execute(
+    	'''
+        SELECT 
+    	    twitter_user_twitter_id,
+    	    tweet_timestamp,
+            twitter_tweet_id,
+            tweet_text,
+            tweet_language,
+            tweet_retweet_count,
+            tweet_place,
+            tweet_user_mention_count,
+            tweet_hashtag_mention_count,
+            tweet_url_count
+        FROM tweet_sample_raw
+        ''') 
 
     #loop through columns
-#    for current_row in tweet_set:
+    for current_row in tweet_set:
     
         #INSERT statement
-#        sql_insert_tweet = '''
-#            INSERT INTO Tweet (twitter_user_twitter_id,
-#                tweet_timestamp,
-#                twitter_tweet_id,
-#                tweet_text,
-#                tweet_language,
-#                tweet_retweet_count,
-#                tweet_place,
-#                tweet_user_mention_count,
-#                tweet_hashtag_mention_count,
-#                tweet_url_count
-#            	)
-#            VALUES (?,?,?,?,?,?,?,?,?,?);
-#            '''
-    	#execute insert function
-#        cursor2.execute(sql_insert_tweet,(current_row[0], current_row[1], current_row[2],
-#            current_row[3], current_row[4], current_row[5], current_row[6], current_row[7],
-#            current_row[8], current_row[9]))
+        sql_insert_tweet = '''
+            INSERT INTO Tweet (twitter_user_twitter_id,
+                tweet_timestamp,
+                twitter_tweet_id,
+                tweet_text,
+                tweet_language,
+                tweet_retweet_count,
+                tweet_place,
+                tweet_user_mention_count,
+                tweet_hashtag_mention_count,
+                tweet_url_count
+            	)
+            VALUES (?,?,?,?,?,?,?,?,?,?);
+            '''
+	   	#execute insert function
+        cursor2.execute(sql_insert_tweet,(current_row[0], current_row[1], current_row[2],
+            current_row[3], current_row[4], current_row[5], current_row[6], current_row[7],
+            current_row[8], current_row[9]))
     	
-        # get ID of each record.
-#        new_tweet_id = cursor2.lastrowid
-    
+       # get ID of each record.
+        new_tweet_id = cursor2.lastrowid
+   
         # output result
-#        print("New record inserted with ID = " + str(new_tweet_id))
+        print("New record inserted with ID = " + str(new_tweet_id))
 
-#        print current_row[0]
+        print current_row[0]
 
     	#commit
-#    	 connection.commit()    
+        connection.commit()    
 
    #END LOOP 
 
 #######################################FILL IN HASHTAG TABLE###########################
 
     #declare variables
-#    hashtag_set = None
-#    current_row = None
-#    sql_insert_hashtag = ""
-#    hashtag_list = []
-#    unique_hashtags = []
+    hashtag_set = None
+    current_row = None
+    sql_insert_hashtag = ""
+    hashtag_list = []
+    unique_hashtags = []
 
     #SELECT statement
-#    hashtag_set = cursor.execute(
-#        '''
-#        SELECT tweet_hashtags_mentioned
-#        FROM tweet_sample_raw
-#        WHERE tweet_hashtags_mentioned !='';
-#        ''') 
+    hashtag_set = cursor.execute(
+        '''
+        SELECT tweet_hashtags_mentioned
+        FROM tweet_sample_raw
+        WHERE tweet_hashtags_mentioned !='';
+        ''') 
     
     #INSERT function
-#    sql_insert_hashtag = '''
-#        INSERT INTO Hashtag 
-#        (hashtag_text)            	
-#        VALUES (?);
-#    '''
+    sql_insert_hashtag = '''
+        INSERT INTO Hashtag 
+        (hashtag_text)            	
+        VALUES (?);
+    '''
 
     #loop over rows of hashags
-#    for current_row in hashtag_set:
+    for current_row in hashtag_set:
 
         #split list
-#        hashtag_list = current_row[0].split(",")
+        hashtag_list = current_row[0].split(",")
 
         #loop through split hashtags
-#        for hashtag in hashtag_list:
+        for hashtag in hashtag_list:
 
             #check if we already have this hashtag
-#            if hashtag not in unique_hashtags:
+            if hashtag not in unique_hashtags:
 
                 #add to list
-#                unique_hashtags.append(hashtag)
+                unique_hashtags.append(hashtag)
 
                 #execute insert function
-#                cursor2.execute(sql_insert_hashtag,(hashtag,))
+                cursor2.execute(sql_insert_hashtag,(hashtag,))
     	
                 #check whether it worked
-#    	        new_hashtag_id = cursor2.lastrowid
+    	        new_hashtag_id = cursor2.lastrowid
     
                 # output result
-#                print("New record inserted with ID = " + str(new_hashtag_id))
+                print("New record inserted with ID = " + str(new_hashtag_id))
 
                 #commit
-#                connection.commit()
+                connection.commit()
 
-            #if already in list do nothing
-#            else:
+            #if already in list tell me it's there
+            else:
 
-#                pass
+                print("got this one already")
+
             #end check            
         #end loop
 
@@ -367,58 +367,59 @@ try:
 #######################################FILL IN URL TABLE###########################
 
     #declare variables
-#    full_url_set = None
-#    current_row = None
-#    sql_insert_full_url = ""
-#    full_url_list = []
-#    unique_full_urls = []
+    full_url_set = None
+    current_row = None
+    sql_insert_full_url = ""
+    full_url_list = []
+    unique_full_urls = []
  
     #SELECT statement
-#    full_url_set = cursor.execute(
-#        '''
-#        SELECT tweet_full_urls_mentioned
-#        FROM tweet_sample_raw
-#        WHERE tweet_full_urls_mentioned !='';
-#        ''') 
+    full_url_set = cursor.execute(
+        '''
+        SELECT tweet_full_urls_mentioned
+        FROM tweet_sample_raw
+        WHERE tweet_full_urls_mentioned !='';
+        ''') 
     
     #INSERT function
-#    sql_insert_full_url = '''
-#       INSERT INTO URL 
-#       (full_url)            	
-#        VALUES (?);
-#    '''
+    sql_insert_full_url = '''
+       INSERT INTO URL 
+       (full_url)            	
+        VALUES (?);
+    '''
 
     #loop over rows of urls
-#    for current_row in full_url_set:
+    for current_row in full_url_set:
 
         #split list
-#        full_url_list = current_row[0].split(",")
+        full_url_list = current_row[0].split(",")
 
-        #loop through split hashtags
-#        for url in full_url_list:
+        #loop through split urls
+        for url in full_url_list:
 
             #check if we already have this url
-#            if url not in unique_full_urls:
+            if url not in unique_full_urls:
 
                 #add to list
-#                unique_full_urls.append(url)
+                unique_full_urls.append(url)
 
                 #execute insert function
-#                cursor2.execute(sql_insert_full_url,(url,))
+                cursor2.execute(sql_insert_full_url,(url,))
     	
                 #check whether it worked
-#    	        new_url_id = cursor2.lastrowid
+    	        new_url_id = cursor2.lastrowid
     
                 # output result
-#                print("New record inserted with ID = " + str(new_url_id))
+                print("New record inserted with ID = " + str(new_url_id))
 
                 #commit
-#                connection.commit()
+                connection.commit()
 
-            #if already in list do nothing
-#            else:
+            #if already in list print message
+            else:
 
-#                pass
+                print ("got this one already")
+
             #end check            
         #end loop
 
