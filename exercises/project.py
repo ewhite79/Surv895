5735d58279f8ce9834d4d2fef4dcfca4ff3a7cdf
Emma White <ewhite79@hotmail.com>
2015-04-05 17:04:20 -0400

    Code to pull url, author, and article text into SQL database

--- exercises/project.py
+++ exercises/project.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import mechanize
 import requests
 
@@ -5,6 +7,10 @@ import requests
 from selenium import webdriver
 
 from bs4 import BeautifulSoup
+
+#import projects for working with SQLite
+import sqlite3
+import six
 
 # declare variables
 test_url = ""
@@ -22,12 +28,20 @@ phantomjs_bin_path = ""
 driver = None
 
 # declare variables - BeautifulSoup
-test_bs = None
+soup = None
 mlive_comment_div_bs = None
+article_text_bs = None
+link_list_bs = None
+link_bs = None
+
+#declare variables - for SQL
+url = ""
+author = ""
+article_text = ""
 
 # if PhantomJS, set the path to the phantomjs executable.
 
-phantomjs_bin_path = "C:/Users/Emma/node_modules/phantomjs/bin/phantomjs"
+phantomjs_bin_path = "C:/Users/Emma/phantomjs-2.0.0-windows/bin/phantomjs.exe"
 
 # example from Windows (must be forward slashes between directories, even though it is Windows):
 #phantomjs_bin_path = "C:/Users/jonathanmorgan/AppData/Roaming/npm/node_modules/phantomjs/lib/phantom/phantomjs.exe"
@@ -76,9 +90,68 @@ elif( http_client == HTTP_PHANTOM_JS ):
 #-- END check to see what request loader we are using. --#
     
 # place HTML in BeautifulSoup
-test_bs = BeautifulSoup( test_html, "html5lib" )
+soup = BeautifulSoup( test_html, "html5lib" )
+
+#get article author 
+article_author_bs = soup.find("span", "author vcard").string
+
+#get article text
+article_text_bs = soup.find ("div", "entry-content")
+article_text = article_text_bs.get_text()
+
+
+#try except to connect to database and put article info into table
+try: 
+
+    #connect to database 
+    connection = sqlite3.connect( "mlivecomments.sqlite" )
+
+    # set row_factory that returns values mapped to column names as well as list
+    connection.row_factory = sqlite3.Row
+
+    #make cursors
+    cursor = connection.cursor()
+    cursor2 = connection.cursor()
+
+    #sql insert statement
+    sql_insert_article = '''
+        INSERT INTO article_raw (
+            url,
+            author,
+            article_text
+            )
+        VALUES (?,?,?);
+    '''
+        
+        
+    #execute insert statement
+    cursor.execute(sql_insert_article,(test_url, article_author_bs, article_text))
+
+    # get ID of each record.
+    new_user_id = cursor.lastrowid
+    
+    # output result
+    print("New record inserted with ID = " + str(new_user_id))
+
+    #commit
+    connection.commit()
+
+except Exception as e:
+    
+    print( "exception: " + str( e ) )
+    
+finally:
+
+   # close cursor
+    cursor.close()
+    cursor2.close()
+
+    # close connection
+    connection.close()
+
+#-- END try-->except-->finally around database access. --#
 
 # get div that contains comments by ID
-mlive_comment_div_bs = test_bs.find( "div", id="rtb-comments")
-print( "- <div id=\"rtb-comments\">: " + str( mlive_comment_div_bs ) )
+#mlive_comment_div_bs = test_bs.find( "div", id="rtb-comments")
+#print( "- <div id=\"rtb-comments\">: " + str( mlive_comment_div_bs ) )
 #print( test_bs )
